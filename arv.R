e00d734fdd2299fc5b81d954afed063e02db78ce
Emmanuel Adeyemo <eadeyemo2@gmail.com>
2023-03-07 19:27:55 -0600

    Update arv.R
    Columns of interest can now be specified instead of just calc ARV on all numeric columns

--- arv.R
+++ arv.R
@@ -1,20 +1,42 @@
 # For calculating ARVs especially for genesyt exports
 
-arv <- function(dat){
+library(crayon)
+library(ggplot2)
+
+#'@param dat - dataset 
+#'@param colums_for_arv - column names of interest for calc ARV
+
+do.arv <- function(x, dat){
   
-  cols <- colnames(dat)
-  for(i in 1:ncol(dat)){
-    
-    x <- cols[i]
-    if(is.numeric(dat[,i])){
-      dat[sapply(dat, is.numeric)]
-      x_name <- paste0(x, "_arv")
-      dat[,x_name] <-  round((100+(sapply(dat[,x], mean) - mean(dat[,x]))/sd(dat[,x])*10),0)
-    }
+  if(!is.numeric(dat[,x])){
+    stop(cat(blue("The specified column name -", x, "- is not numeric.\n")))
   }
   
- 
-  return(dat)
+  out_col_name <- paste0(x, "_arv")
+  solve_col <-  round((100+(sapply(dat[,x], mean) - mean(dat[,x]))/sd(dat[,x])*10),0)
+  solve_col <- as.data.frame(solve_col)
+  colnames(solve_col) <- out_col_name
+  
+  return(solve_col)
 }
 
-arv(test)
+
+out.arv <- function(dat, columns_for_arv){
+  
+  arv_res <- lapply(columns_for_arv, do.arv, dat)
+  
+  arv_bind <- bind_cols(arv_res)
+  
+  dt_arv <- cbind(dat, arv_bind)
+}
+
+
+### example with mpg dataset from the ggplot2 library
+d <- as.data.frame(mpg)
+
+sapply(mpg, class)
+
+col_of_interest <- c("displ", "cty", "hwy")
+
+ex <- out.arv(d, col_of_interest)
+
